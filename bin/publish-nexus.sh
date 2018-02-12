6ff8e0629ee2344a59c71e9b10dcfdce7b4611cc
Geert Premereur <geert.premereur+awv@ictects.com>
2018-02-12 15:22:26 +0100

    AGL-2694: Stijldefinities
    aangepast: fix publish script debug commandos

--- bin/publish-nexus.sh
+++ bin/publish-nexus.sh
@@ -6,10 +6,10 @@ echo ""
 . $(dirname "$0")/_init.sh
 cd $BASEDIR/dist
 if [ ! -z $BAMBOO_AGENT_HOME ]; then
-  echo bouwen onder bamboo `config get registry`
+  echo bouwen onder bamboo `npm config get registry`
   npm config get registry
   npm config set registry https://collab.mow.vlaanderen.be/artifacts/repository/npm-internal/ 
-  echo bouwen onder bamboo `config get registry`
+  echo bouwen onder bamboo `npm config get registry`
   npm publish
 else
   echo "Lokaal kan je niet naar Nexus publishen, enkel Bamboo kan dat"
