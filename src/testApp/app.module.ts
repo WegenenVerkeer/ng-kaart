f7ed29caeac333a53c1f804d9d37fe44b9c618ff
Geert Premereur <bepremeg@users.noreply.github.com>
2018-04-20 15:23:11 +0200

    AGL-2656: Locatiezoeker
    aangepast: configuratie op een manier die AOT overleeft

--- src/testApp/app.module.ts
+++ src/testApp/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClickOutsideModule } from "ng4-click-outside";
 
 import { KaartModule } from "../lib/kaart/index";
-import { AppComponent } from "./app.component";
-import { FormsModule } from "@angular/forms";
 import { ZoekerModule } from "../lib/zoeker/index";
-import { HttpModule } from "@angular/http";
-import { ClickOutsideModule } from "ng4-click-outside";
-import { AvKaartComponent } from "./av-kaart.component";
+import { AppComponent } from "./app.component";
 import { AvKaartInnerComponent } from "./av-kaart-inner.component";
-import { GoogleLocatieZoekerConfig } from "../lib/zoeker/google-locatie-zoeker.config";
+import { AvKaartComponent } from "./av-kaart.component";
 
 @NgModule({
   declarations: [AppComponent, AvKaartComponent, AvKaartInnerComponent],
