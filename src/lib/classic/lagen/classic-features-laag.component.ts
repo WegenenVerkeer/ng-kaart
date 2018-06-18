b5136fc66ee31251d4276b659a8b1db7dcd325f8
Geert Premereur <bepremeg@users.noreply.github.com>
2018-06-18 12:20:55 +0200

    AGL-2980: VervangFeaturesCmd te vroeg
    aangepast: betere & configureerbare gelijkheidstest.

--- src/lib/classic/lagen/classic-features-laag.component.ts
+++ src/lib/classic/lagen/classic-features-laag.component.ts
@@ -11,12 +11,27 @@ import { logOnlyWrapper } from "../messages";
 
 import { ClassicVectorLaagComponent } from "./classic-vector-laag.component";
 
-// Hier gaan we er van uit dat de feature zelf immutable is. Features moeten ook unieke IDs hebben.
-const setoidFeature = {
-  equals: (feat1: ol.Feature, feat2: ol.Feature) => feat1.getId === feat2.getId
+const id = (feat: ol.Feature) => {
+  const featId = feat.getId();
+  if (featId !== undefined) {
+    return featId;
+  } else {
+    const props = feat.getProperties();
+    if (props !== undefined && props !== null) {
+      return props["id"];
+    } else {
+      return undefined;
+    }
+  }
 };
 
-const setoidFeatureArray = array.getSetoid(setoidFeature);
+// Hier gaan we er van uit dat de feature zelf immutable is. Features moeten ook unieke IDs hebben.
+// De ids kunnen expliciet gezet zijn met ol.feature#setId of in de properties array staan.
+const stdFeatureEquality = (feat1: ol.Feature, feat2: ol.Feature) => {
+  const id1 = id(feat1);
+  const id2 = id(feat2);
+  return id1 !== undefined && id2 !== undefined && id1 === id2;
+};
 
 @Component({
   selector: "awv-kaart-features-laag",
@@ -24,7 +39,9 @@ const setoidFeatureArray = array.getSetoid(setoidFeature);
   encapsulation: ViewEncapsulation.None
 })
 export class ClassicFeaturesLaagComponent extends ClassicVectorLaagComponent implements DoCheck {
+  // Er wordt verwacht dat alle features een unieke ID hebben. Null of undefined ID's zorgen voor veel onnodige updates.
   @Input() features = [] as ol.Feature[];
+  @Input() featureEquality: (feat1: ol.Feature, feat2: ol.Feature) => boolean = stdFeatureEquality;
 
   // TODO combineren met 'selecteerbaar' van kaart-vector-laag
   @Output() featureGeselecteerd: EventEmitter<ol.Feature> = new EventEmitter<ol.Feature>();
@@ -48,7 +65,11 @@ export class ClassicFeaturesLaagComponent extends ClassicVectorLaagComponent imp
   ngDoCheck(): void {
     // deze check ipv ngOnChanges want wijzigingen aan features staan niet in SimpleChanges
     forEach(this.vorigeFeatures, features => {
-      if (!setoidFeatureArray.equals(this.features, features)) {
+      if (
+        !array
+          .getSetoid({ equals: (feat1: ol.Feature, feat2: ol.Feature) => this.featureEquality(feat1, feat2) })
+          .equals(this.features, features)
+      ) {
         this.dispatchVervangFeatures(this.features);
       }
     });
