436603781ca82bd5164f3eac94cc2cdf367b9fcd
Natal Vande Casteele <natal@libius.be>
2018-07-10 13:22:33 +0200

    AGL-3031: missing export

--- src/lib/stijl/index.ts
+++ src/lib/stijl/index.ts
@@ -2,3 +2,4 @@ export * from "./stijl-static";
 export * from "./stijl-function";
 export * from "./stijl-function-types";
 export * from "./offset-stijl-function";
+export * from "./verkeersborden-stijl-function";
