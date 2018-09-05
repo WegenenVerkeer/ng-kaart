ec22066599be31d85146adb1c7516b88dd8f80af
bauwelp <bauwelp@gmail.com>
2018-09-05 11:10:25 +0200

    AGL-3149: Als je een laag verwijderd terwijl deze nog aan het laden is, blijft de applicatie deze laden

--- src/lib/kaart/loading/kaart-loading.component.ts
+++ src/lib/kaart/loading/kaart-loading.component.ts
@@ -8,7 +8,6 @@ import {
   distinctUntilChanged,
   map,
   mergeAll,
-  scan,
   shareReplay,
   startWith,
   switchMap,
@@ -21,7 +20,7 @@ import { observeOnAngular } from "../../util/observe-on-angular";
 import { ofType } from "../../util/operators";
 import { KaartChildComponentBase } from "../kaart-child-component-base";
 import { isNoSqlFsLaag, ToegevoegdeLaag, VectorLaag } from "../kaart-elementen";
-import { DataLoadEvent, LoadError } from "../kaart-load-events";
+import { DataLoadEvent, LoadComplete, LoadError } from "../kaart-load-events";
 import * as prt from "../kaart-protocol";
 import { KaartComponent } from "../kaart.component";
 
@@ -50,9 +49,22 @@ export class KaartLoadingComponent extends KaartChildComponentBase {
     const lagenLaag$$: rx.Observable<List<rx.Observable<DataLoadEvent>>> = this.modelChanges.lagenOpGroep
       .get("Voorgrond.Laag")
       .pipe(map(noSqlFsLagenDataLoadEvents), startWith(List()));
+
     const toegevoegdeLagen$$: rx.Observable<List<rx.Observable<DataLoadEvent>>> = lagenHoog$$.pipe(
       combineLatest(lagenLaag$$, (lgnHg, lgnLg) => lgnHg.concat(lgnLg).toList())
     );
+
+    const busy$: rx.Observable<boolean> = toegevoegdeLagen$$.pipe(
+      // We moeten de dataloadEvents hier doen starten met een LoadComplete event,
+      // want het kan zijn dat er lagen zijn die al gereed zijn en dus nooit een event gaan uitsturen,
+      // waardoor de combineLatest ook niks gaat uitsturen.
+      switchMap(lagen => rx.Observable.combineLatest(lagen.map(laag => laag!.pipe(startWith(LoadComplete as DataLoadEvent))).toArray())),
+      // Als het laaste event voor een laag LoadStart of PartReceived is, is de laag nog bezig met laden.
+      map(lastStates => lastStates.filter(state => state.type === "LoadStart" || state.type === "PartReceived").length > 0),
+      startWith(false),
+      distinctUntilChanged()
+    );
+
     const mergedDataloadEvent$: rx.Observable<DataLoadEvent> = toegevoegdeLagen$$.pipe(
       // subscribe/unsubscribe voor elke nieuwe lijst van toegevoegde lagen
       switchMap(
@@ -62,25 +74,8 @@ export class KaartLoadingComponent extends KaartChildComponentBase {
       ),
       shareReplay(1, 1000)
     );
-    const numBusy$: rx.Observable<number> = mergedDataloadEvent$.pipe(
-      scan((numBusy: number, evt: DataLoadEvent) => {
-        switch (evt.type) {
-          case "LoadStart":
-            return numBusy + 1;
-          case "LoadComplete":
-            return numBusy - 1;
-          case "LoadError":
-            return numBusy - 1;
-          case "PartReceived":
-            // We doen hier dus niks mee, maar we zouden ook een tick kunnen geven.
-            // Echter maar interessant als we het totaal aantal chunks kennen.
-            return numBusy;
-        }
-      }, 0)
-    );
     const stableError$ = (stability: number) => mergedDataloadEvent$.pipe(ofType<LoadError>("LoadError"), debounceTime(stability));
 
-    const busy$: rx.Observable<boolean> = numBusy$.pipe(map(numBusy => numBusy > 0), distinctUntilChanged(), shareReplay(1));
     const inError$: rx.Observable<boolean> = stableError$(100).pipe(
       switchMapTo(rx.Observable.timer(0, 1000).pipe(map(t => t === 0), take(2))), // Produceert direct true, dan na een seconde false
       startWith(false)
