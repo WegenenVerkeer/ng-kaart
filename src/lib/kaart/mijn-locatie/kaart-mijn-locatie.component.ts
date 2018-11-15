df9f07d2e7b737a15e07d12fbdde7ec9f686b480
Natal Vande Casteele <natal@libius.be>
2018-11-15 20:50:04 +0100

    AGL-3250: tracking modus geintroduceerd

--- src/lib/kaart/mijn-locatie/kaart-mijn-locatie.component.ts
+++ src/lib/kaart/mijn-locatie/kaart-mijn-locatie.component.ts
@@ -1,59 +1,124 @@
 import { AfterViewInit, Component, NgZone, OnInit, QueryList, ViewChildren } from "@angular/core";
 import { MatButton } from "@angular/material";
+import { Function2 } from "fp-ts/lib/function";
 import { none, Option, some } from "fp-ts/lib/Option";
 import { List, OrderedMap } from "immutable";
 import * as ol from "openlayers";
 import * as rx from "rxjs";
-import { map, mapTo, switchMap } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { distinctUntilChanged, map, switchMap, take } from "rxjs/operators";
 
 import { flatten } from "../../util/operators";
-import { KaartChildComponentBase } from "../kaart-child-component-base";
-import * as ke from "../kaart-elementen";
 import { VeldInfo } from "../kaart-elementen";
-import { kaartLogOnlyWrapper } from "../kaart-internal-messages";
+import * as ke from "../kaart-elementen";
+import { actieveModusGezetWrapper, KaartInternalMsg, kaartLogOnlyWrapper } from "../kaart-internal-messages";
+import { KaartModusComponent } from "../kaart-modus-component";
 import * as prt from "../kaart-protocol";
 import { Viewinstellingen } from "../kaart-protocol";
 import { KaartComponent } from "../kaart.component";
 import { kaartLogger } from "../log";
-import * as ss from "../stijl-selector";
 
 export const MijnLocatieUiSelector = "Mijnlocatie";
 const MijnLocatieLaagNaam = "Mijn Locatie";
+
+interface Zoom {
+  zoom: number;
+  doel: number;
+}
+
+interface Locatie extends Zoom {
+  positie: Position;
+}
+
+interface Activatie extends Zoom {
+  actief: boolean;
+}
+
+const Zoom: Function2<number, number, Zoom> = (zoom, doel) => ({ zoom: zoom, doel: doel });
+const Locatie: Function2<Zoom, Position, Locatie> = (zoom, positie) => ({ ...zoom, positie: positie });
+const Activatie: Function2<Zoom, boolean, Activatie> = (zoom, actief) => ({ ...zoom, actief: actief });
+
+const pasFeatureAan = (feature: ol.Feature, coordinate: ol.Coordinate, zoom: number, accuracy: number): Option<ol.Feature> => {
+  feature.setGeometry(new ol.geom.Point(coordinate));
+  zetStijl(feature, zoom, accuracy);
+  return some(feature);
+};
+
+const zetStijl = (feature: ol.Feature, zoom: number, accuracy: number): void => feature.setStyle(mijnLocatieStijl(zoom, accuracy));
+
+const mijnLocatieStijl = (zoom: number, accuracy: number): ol.style.Style => {
+  // TODO: resolutions moet uit kaart komen
+  const resolutions = [1024.0, 512.0, 256.0, 128.0, 64.0, 32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125];
+  const accuracyInPixels = accuracy / resolutions[zoom - 1];
+  const radius = Math.max(accuracyInPixels, 4);
+  return new ol.style.Style({
+    image: new ol.style.Circle({
+      fill: new ol.style.Fill({
+        color: [65, 105, 225, 0.1]
+      }),
+      stroke: new ol.style.Stroke({
+        color: [65, 105, 225, 1],
+        width: 1.25
+      }),
+      radius: radius
+    })
+  });
+};
 
 @Component({
   selector: "awv-kaart-mijn-locatie",
   templateUrl: "./kaart-mijn-locatie.component.html",
   styleUrls: ["./kaart-mijn-locatie.component.scss"]
 })
-export class KaartMijnLocatieComponent extends KaartChildComponentBase implements OnInit, AfterViewInit {
+export class KaartMijnLocatieComponent extends KaartModusComponent implements OnInit, AfterViewInit {
   private viewinstellingen$: rx.Observable<Viewinstellingen> = rx.empty();
   private zoomdoelSetting$: rx.Observable<Option<number>> = rx.empty();
+  private activeerSubj: Subject<boolean> = new Subject<boolean>();
+  private locatieSubj: Subject<Position> = new Subject<Position>();
 
   enabled$: rx.Observable<boolean> = rx.of(true);
 
   @ViewChildren("locateBtn")
   locateBtnQry: QueryList<MatButton>;
 
-  mijnLocatieStyle: ol.style.Style;
-  mijnLocatie: Option<ol.Feature> = none;
+  private mijnLocatie: Option<ol.Feature> = none;
+  private watchId: Option<number> = none;
 
-  static pasFeatureAan(feature: ol.Feature, coordinate: ol.Coordinate): Option<ol.Feature> {
-    feature.setGeometry(new ol.geom.Point(coordinate));
-    return some(feature);
+  modus(): string {
+    return MijnLocatieUiSelector;
+  }
+
+  isDefaultModus() {
+    return false;
+  }
+
+  // geactiveerd van buiten af
+  activeer(active: boolean) {
+    this.actief = active;
+    this.activeerSubj.next(this.actief);
+  }
+
+  public get isActief(): boolean {
+    return this.actief;
+  }
+
+  protected kaartSubscriptions(): prt.Subscription<KaartInternalMsg>[] {
+    return [prt.ActieveModusSubscription(actieveModusGezetWrapper)];
+  }
+
+  // activatie geinitieerd door gebruiker
+  toggleLocatieTracking(): void {
+    this.actief = !this.actief;
+    this.activeerSubj.next(this.actief);
+    if (this.actief) {
+      this.publiceerActivatie();
+    } else {
+      this.publiceerDeactivatie();
+    }
   }
 
   constructor(zone: NgZone, private readonly parent: KaartComponent) {
     super(parent, zone);
-    this.mijnLocatieStyle = new ol.style.Style({
-      image: new ol.style.Icon({
-        anchor: [0.5, 0.5],
-        anchorXUnits: "fraction",
-        anchorYUnits: "fraction",
-        scale: 0.5,
-        color: "#00a2c5",
-        src: require("material-design-icons/maps/2x_web/ic_my_location_white_18dp.png")
-      })
-    });
   }
 
   ngOnInit(): void {
@@ -73,30 +138,39 @@ export class KaartMijnLocatieComponent extends KaartChildComponentBase implement
     this.viewinstellingen$ = this.parent.modelChanges.viewinstellingen$;
     this.zoomdoelSetting$ = this.parent.modelChanges.mijnLocatieZoomDoel$;
     this.enabled$ = this.zoomdoelSetting$.pipe(map(m => m.isSome()));
-  }
 
-  ngAfterViewInit() {
-    super.ngAfterViewInit();
     const zoomdoel$: rx.Observable<number> = this.zoomdoelSetting$.pipe(flatten); // Hou enkel de effectieve zoomniveaudoelen over
+    const zoom$ = this.viewinstellingen$.pipe(
+      distinctUntilChanged((vi1, vi2) => vi1.zoom === vi2.zoom && vi1.minZoom === vi2.minZoom && vi1.maxZoom === vi2.maxZoom),
+      map(vi => vi.zoom)
+    );
 
     this.bindToLifeCycle(
-      // Omdat de button in een ngIf zit, moeten we op zoek naar de button in ngAfterViewInit
-      this.locateBtnQry.changes.pipe(
-        switchMap(ql =>
-          rx.combineLatest(this.viewinstellingen$, zoomdoel$, (zi, doel) => [zi.zoom, doel]).pipe(
-            // Blijf op de hoogte van huidige en gewenste zoom
-            switchMap(params => rx.fromEvent(ql.first._getHostElement(), "click").pipe(mapTo(params))) // van click naar zoom
+      this.activeerSubj.pipe(
+        switchMap(actief =>
+          rx.combineLatest([zoom$, zoomdoel$]).pipe(
+            take(1),
+            map(([zoom, doel]) => Activatie(Zoom(zoom, doel), actief))
           )
         )
       )
-    ).subscribe(([zoom, doelniveau]) => this.zoomNaarMijnLocatie(zoom, doelniveau));
+    ).subscribe(zoomActief => (zoomActief.actief ? this.startTracking(zoomActief.zoom, zoomActief.doel) : this.stopTracking()));
+
+    this.bindToLifeCycle(
+      rx.combineLatest(zoom$, zoomdoel$, this.locatieSubj).pipe(map(([zoom, doel, locatie]) => Locatie(Zoom(zoom, doel), locatie)))
+    ).subscribe(zoomPositie => this.zetMijnPositie(zoomPositie.positie, zoomPositie.zoom, zoomPositie.doel));
   }
 
-  private maakNieuwFeature(coordinate: ol.Coordinate): Option<ol.Feature> {
+  private maakNieuwFeature(coordinate: ol.Coordinate, zoom: number, accuracy: number): Option<ol.Feature> {
     const feature = new ol.Feature(new ol.geom.Point(coordinate));
-    feature.setStyle(this.mijnLocatieStyle);
+    feature.setStyle(mijnLocatieStijl(zoom, accuracy));
     this.dispatch(prt.VervangFeaturesCmd(MijnLocatieLaagNaam, List.of(feature), kaartLogOnlyWrapper));
     return some(feature);
+  }
+
+  private verwijderFeature() {
+    this.mijnLocatie = none;
+    this.dispatch(prt.VervangFeaturesCmd(MijnLocatieLaagNaam, List(), kaartLogOnlyWrapper));
   }
 
   private meldFout(fout: PositionError | string) {
@@ -108,32 +182,44 @@ export class KaartMijnLocatieComponent extends KaartChildComponentBase implement
     );
   }
 
-  private zoomNaarMijnLocatie(zoom: number, doelzoom: number) {
+  private stopTracking() {
+    this.watchId.map(watchId => navigator.geolocation.clearWatch(watchId));
+    this.verwijderFeature();
+  }
+
+  private startTracking(zoom: number, doelzoom: number) {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(positie => this.zetMijnPositie(positie, zoom, doelzoom), fout => this.meldFout(fout), {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 50000
-      });
+      if (zoom <= 8) {
+        // We zitten nu op een te laag zoomniveau, dus gaan we eerst inzoomen.
+        this.dispatch(prt.VeranderZoomCmd(doelzoom, kaartLogOnlyWrapper));
+      }
+
+      this.watchId = some(
+        navigator.geolocation.watchPosition(
+          //
+          positie => this.locatieSubj.next(positie),
+          fout => this.meldFout(fout),
+          {
+            enableHighAccuracy: true,
+            timeout: 1000,
+            maximumAge: 0
+          }
+        )
+      );
     } else {
       this.meldFout("Geen geolocatie mogelijk");
     }
   }
 
   private zetMijnPositie(position: Position, zoom: number, doelzoom: number) {
-    if (zoom <= 2) {
-      // We zitten nu op heel Vlaanderen, dus gaan we eerst inzoomen.
-      this.dispatch(prt.VeranderZoomCmd(doelzoom, kaartLogOnlyWrapper));
-    }
-
     const longLat: ol.Coordinate = [position.coords.longitude, position.coords.latitude];
 
     const coordinate = ol.proj.fromLonLat(longLat, "EPSG:31370");
     this.dispatch(prt.VeranderMiddelpuntCmd(coordinate));
 
     this.mijnLocatie = this.mijnLocatie
-      .chain(feature => KaartMijnLocatieComponent.pasFeatureAan(feature, coordinate))
-      .orElse(() => this.maakNieuwFeature(coordinate));
+      .chain(feature => pasFeatureAan(feature, coordinate, zoom, position.coords.accuracy))
+      .orElse(() => this.maakNieuwFeature(coordinate, zoom, position.coords.accuracy));
   }
 
   createLayer(): ke.VectorLaag {
@@ -141,7 +227,7 @@ export class KaartMijnLocatieComponent extends KaartChildComponentBase implement
       type: ke.VectorType,
       titel: MijnLocatieLaagNaam,
       source: new ol.source.Vector(),
-      styleSelector: some(ss.StaticStyle(this.mijnLocatieStyle)),
+      styleSelector: none,
       styleSelectorBron: none,
       selectieStyleSelector: none,
       hoverStyleSelector: none,
