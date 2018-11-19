6ac1f7eda95bf994007e471ff79726b59fa66ea7
bauwelp <bauwelp@gmail.com>
2018-11-19 13:54:24 +0100

    AGL-3280: travis deploy

--- projects/ng-kaart/karma.conf.js
+++ projects/ng-kaart/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false,
 
     // you can define custom flags
