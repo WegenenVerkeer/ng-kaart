c911ddcc6001c9ace8f9c3a05b335450e748410d
Natal Vande Casteele <natal@libius.be>
2018-11-22 20:06:36 +0100

    AGL-3249: knop verschijnt bij rotatie, weg bij klikken

--- projects/ng-kaart/src/lib/kaart/rotatie/kaart-rotatie.component.ts
+++ projects/ng-kaart/src/lib/kaart/rotatie/kaart-rotatie.component.ts
@@ -3,7 +3,7 @@ import { MatIconRegistry } from "@angular/material";
 import { DomSanitizer } from "@angular/platform-browser";
 import { some } from "fp-ts/lib/Option";
 import * as rx from "rxjs";
-import { distinctUntilChanged, map, tap } from "rxjs/operators";
+import { delay, distinctUntilChanged, map, tap } from "rxjs/operators";
 
 import { encodeAsSvgUrl } from "../../util/url";
 import { KaartChildComponentBase } from "../kaart-child-component-base";
@@ -21,31 +21,35 @@ const compassSVG =
   styleUrls: ["./kaart-rotatie.component.scss"]
 })
 export class KaartRotatieComponent extends KaartChildComponentBase implements OnInit {
-  readonly zetRotatieSubj: rx.Subject<number> = new rx.Subject<number>();
   readonly rotatie$: rx.Observable<number>;
+  readonly zichtbaar$: rx.Observable<boolean>;
+  readonly clickBtnSubj: rx.Subject<boolean> = new rx.Subject<boolean>();
 
   constructor(
     private readonly parent: KaartComponent,
     zone: NgZone,
     private readonly matIconRegistry: MatIconRegistry,
-    private readonly sanitizer: DomSanitizer,
-    private readonly cd: ChangeDetectorRef
+    private readonly domSanitize: DomSanitizer,
+    private readonly changeDector: ChangeDetectorRef
   ) {
     super(parent, zone);
 
-    this.matIconRegistry.addSvgIcon("compass", this.sanitizer.bypassSecurityTrustResourceUrl(encodeAsSvgUrl(compassSVG)));
+    this.matIconRegistry.addSvgIcon("compass", this.domSanitize.bypassSecurityTrustResourceUrl(encodeAsSvgUrl(compassSVG)));
 
     this.rotatie$ = this.parent.modelChanges.rotatie$.pipe(
-      distinctUntilChanged((rot1, rot2) => rot1 === rot2),
-      tap(() => this.cd.detectChanges()) // force redraw rotated compass
+      distinctUntilChanged(),
+      tap(() => this.changeDector.detectChanges()) // force redraw rotated compass, anders wordt die niet dynamisch getekend
     );
 
-    this.bindToLifeCycle(this.zetRotatieSubj).subscribe(rotatie =>
-      this.dispatch(prt.VeranderRotatieCmd(rotatie, some(250), kaartLogOnlyWrapper))
+    this.zichtbaar$ = rx.merge(
+      rx.of(false), // kompas initieel niet zichtbaar
+      this.rotatie$.pipe(map(() => true)), // tot de kaart gedraaid wordt
+      this.bindToLifeCycle(this.clickBtnSubj).pipe(delay(2000)) // en terug onzichtbaar 2 seconden na gebruiker klik op knop
     );
   }
 
-  zetRotatie(rotatie: number) {
-    this.zetRotatieSubj.next(rotatie);
+  roteerNaarNoord() {
+    this.dispatch(prt.VeranderRotatieCmd(0, some(250), kaartLogOnlyWrapper));
+    this.clickBtnSubj.next(false);
   }
 }
