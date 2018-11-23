a18eac367d714b2548c5e9138e6f3837a74a69e3
Natal Vande Casteele <natal@libius.be>
2018-11-23 09:42:39 +0100

    AGL-3249: knop verschijnt bij rotatie, weg bij klikken

--- projects/ng-kaart/src/lib/kaart/rotatie/kaart-rotatie.component.ts
+++ projects/ng-kaart/src/lib/kaart/rotatie/kaart-rotatie.component.ts
@@ -11,6 +11,7 @@ import { kaartLogOnlyWrapper } from "../kaart-internal-messages";
 import * as prt from "../kaart-protocol";
 import { KaartComponent } from "../kaart.component";
 
+// https://materialdesignicons.com/icon/compass
 const compassSVG =
   // tslint:disable-next-line:max-line-length
   '<svg x="0px" y="0px" width="24px" height="24px" transform="rotate(-45)" viewBox="0 0 24 24"><path d="M14.19,14.19L6,18L9.81,9.81L18,6M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,10.9A1.1,1.1 0 0,0 10.9,12A1.1,1.1 0 0,0 12,13.1A1.1,1.1 0 0,0 13.1,12A1.1,1.1 0 0,0 12,10.9Z" /></svg>';
