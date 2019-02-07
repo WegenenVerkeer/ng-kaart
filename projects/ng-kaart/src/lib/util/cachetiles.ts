c5075f2330da520a4d1a3eb01f0c2d2037fe3b3f
Natal Vande Casteele <natal@libius.be>
2019-02-07 13:09:35 +0100

    CK-47: parallele requests & minder progress messages

--- projects/ng-kaart/src/lib/util/cachetiles.ts
+++ projects/ng-kaart/src/lib/util/cachetiles.ts
@@ -7,6 +7,10 @@ import { splitInChunks } from "./arrays";
 
 const AANTAL_PARALLELE_REQUESTS = 6;
 
+/**
+ * Haalt alle urls op met 6 parallele requests door de url array in 6 chunks te verdelen en deze parallel af te lopen.
+ * Elke chunk gaat sequentieel 1 voor 1 elke URL ophalen.
+ */
 const fetchUrlsGrouped = (urls: string[], setProgress: Function1<number, void>) => {
   let fetched = 0;
 
