953f29e59441a1ce2f854cd8383d41ef9cce0499
Natal Vande Casteele <natal@libius.be>
2019-02-19 10:13:42 +0100

    CK-61: opmerkingen na MR

--- projects/ng-kaart/src/lib/source/nosql-fs-source.ts
+++ projects/ng-kaart/src/lib/source/nosql-fs-source.ts
@@ -1,21 +1,17 @@
-import { Function1, Function2, Function3, Function4 } from "fp-ts/lib/function";
+import { array } from "fp-ts";
+import { Function1, Function2 } from "fp-ts/lib/function";
 import { fromNullable, Option } from "fp-ts/lib/Option";
 import * as ol from "openlayers";
 import * as rx from "rxjs";
-import { bufferCount, switchMap } from "rxjs/operators";
+import { bufferCount, filter, last, map, mergeMap, scan, share } from "rxjs/operators";
 
 import * as le from "../kaart/kaart-load-events";
 import { kaartLogger } from "../kaart/log";
-import { fetchWithTimeout } from "../util/fetch-with-timeout";
+import { Pipeable } from "../util";
+import { fetchWithTimeoutObs$ } from "../util/fetch-with-timeout";
+import { ReduceFunction } from "../util/function";
 import * as geojsonStore from "../util/geojson-store";
 import { GeoJsonLike } from "../util/geojson-store";
-
-const FETCH_TIMEOUT = 5000; // max time to wait for data from featureserver before checking cache
-const BATCH_SIZE = 100; // aantal features per keer toevoegen aan laag
-
-const format = new ol.format.GeoJSON();
-const decoder = new TextDecoder();
-const featureDelimiter = "\n";
 
 /**
  * Stappen:
@@ -23,42 +19,69 @@ const featureDelimiter = "\n";
  1. Er komt een extent binnen van de kaart om de features op te vragen
  2. NosqlfsLoader gaat de features voor die extent opvragen aan featureserver
  3. Bij ontvangen van de features worden deze bewaard in een indexeddb (1 per laag), gemanaged door ng-kaart. Moeten bestaande niet eerst
-    verwijderd worden?
+ verwijderd worden?
  4. Indien NosqlfsLoader binnen 5 seconden geen antwoord heeft gekregen, worden eventueel bestaande features binnen de extent uit
-    de indexeddb gehaald
+ de indexeddb gehaald
 
  */
 
-const geoJsonLike: Function1<string, GeoJsonLike> = lijn => {
-  try {
-    const geojson = JSON.parse(lijn) as GeoJsonLike;
-    return {
-      ...geojson,
-      metadata: {
-        minx: geojson.geometry.bbox[0],
-        miny: geojson.geometry.bbox[1],
-        maxx: geojson.geometry.bbox[2],
-        maxy: geojson.geometry.bbox[3],
-        toegevoegd: new Date()
-      }
-    };
-  } catch (error) {
-    kaartLogger.error(`Kon JSON data niet parsen: ${error}`);
-    throw new Error(`Kon JSON data niet parsen: ${error}`);
-  }
+const FETCH_TIMEOUT = 5000; // max time to wait for data from featureserver before checking cache
+const BATCH_SIZE = 100; // aantal features per keer toevoegen aan laag
+
+const format = new ol.format.GeoJSON();
+
+const featureDelimiter = "\n";
+
+interface SplitterState {
+  readonly seen: string;
+  readonly output: string[];
+}
+
+const splitter: Function1<string, ReduceFunction<SplitterState, string>> = delimiter => (state, line) => {
+  const allData = state.seen + line;
+  const parts = allData.split(delimiter);
+  // foldr doet niks meer dan het laatste element van de array nemen en houdt er ok rekening mee dat de array leeg kan zijn
+  return array.foldr(parts, { seen: "", output: [] }, (init, last) => ({ seen: last, output: init }));
 };
 
-const geoJsons: Function1<string[], GeoJsonLike[]> = volledigeLijnen => {
-  try {
-    const features = volledigeLijnen //
-      .filter(lijn => lijn.trim().length > 0) //
-      .map(geoJsonLike);
-    return features!;
-  } catch (error) {
-    kaartLogger.error(`Kon JSON data niet parsen: ${error}`);
-    throw new Error(`Kon JSON data niet parsen: ${error}`);
-  }
+const split: Function1<string, Pipeable<string, string>> = delimiter => obs => {
+  const splitterState$: rx.Observable<SplitterState> = obs.pipe(
+    scan(splitter(delimiter), { seen: "", output: [] }),
+    share()
+  );
+  return rx.merge(
+    splitterState$.pipe(mergeMap<SplitterState, string>(s => rx.from(s.output))),
+    splitterState$.pipe(
+      // we mogen de laatste output niet verliezen
+      last(),
+      filter(s => s.seen.length > 0),
+      map(s => s.seen)
+    )
+  );
 };
+
+const toGeoJson: Pipeable<string, GeoJsonLike> = obs =>
+  obs.pipe(
+    map(lijn => {
+      try {
+        // TODO: safer parsing
+        const geojson = JSON.parse(lijn) as GeoJsonLike;
+        return {
+          ...geojson,
+          metadata: {
+            minx: geojson.geometry.bbox[0],
+            miny: geojson.geometry.bbox[1],
+            maxx: geojson.geometry.bbox[2],
+            maxy: geojson.geometry.bbox[3],
+            toegevoegd: new Date()
+          }
+        };
+      } catch (error) {
+        kaartLogger.error(`Kon JSON data niet parsen: ${error}`);
+        throw new Error(`Kon JSON data niet parsen: ${error}`);
+      }
+    })
+  );
 
 const toOlFeature: Function2<string, GeoJsonLike, ol.Feature> = (laagnaam, geojson) =>
   new ol.Feature({
@@ -67,71 +90,6 @@ const toOlFeature: Function2<string, GeoJsonLike, ol.Feature> = (laagnaam, geojs
     geometry: format.readGeometry(geojson.geometry),
     laagnaam: laagnaam
   });
-
-interface ResponseResult {
-  value: Uint8Array;
-  done: boolean;
-}
-
-const verwerkChunk: Function4<ResponseResult, string, rx.Subscriber<GeoJsonLike>, () => rx.Observable<ResponseResult>, void> = (
-  responseResult,
-  restData,
-  subscriber,
-  getNextResponseObs$
-) => {
-  const nieuweData =
-    restData +
-    decoder.decode(responseResult.value || new Uint8Array(0), {
-      stream: !responseResult.done
-    }); // append nieuwe data (in geval er een half ontvangen lijn is van vorige call)
-
-  const ontvangenLijnen = nieuweData.split(featureDelimiter);
-
-  if (!responseResult.done) {
-    // laatste lijn is vermoedelijk niet compleet. Hou bij voor volgende keer
-    const newRestData = ontvangenLijnen[ontvangenLijnen.length - 1];
-
-    // parse alle lijnen behalve de laatste (vermoedelijk gedeeltelijke lijn)
-    geoJsons(ontvangenLijnen.slice(0, -1)).map(geojson => subscriber.next(geojson));
-
-    // andere data ophalen
-    getNextResponseObs$().subscribe(
-      response => verwerkChunk(response, newRestData, subscriber, getNextResponseObs$),
-      error => subscriber.error(error)
-    );
-  } else {
-    // alle data werd ontvangen, stuur laatste features door
-    geoJsons(ontvangenLijnen).map(geojson => subscriber.next(geojson));
-    subscriber.complete();
-  }
-};
-
-const getReaderResponseObs$: Function1<ReadableStreamReader, rx.Observable<ResponseResult>> = reader =>
-  rx.from<ResponseResult>(reader.read());
-
-const getReceivedFeaturesObs$: Function3<string, string, Response, rx.Observable<GeoJsonLike>> = (
-  collection,
-  featureDelimiter,
-  response
-) => {
-  return rx.Observable.create((subscriber: rx.Subscriber<GeoJsonLike>) => {
-    if (!response.ok) {
-      subscriber.error(`Probleem bij ontvangen nosql ${collection} data: status ${response.status} ${response.statusText}`);
-      return;
-    }
-
-    if (!response.body) {
-      subscriber.error(`Probleem bij ontvangen nosql ${collection} data: response.body is leeg`);
-      return;
-    }
-
-    const reader = response.body.getReader();
-    getReaderResponseObs$(reader).subscribe(
-      response => verwerkChunk(response, "", subscriber, () => getReaderResponseObs$(reader)), //
-      error => subscriber.error(error)
-    );
-  });
-};
 
 export class NosqlFsSource extends ol.source.Vector {
   private readonly loadEventSubj = new rx.Subject<le.DataLoadEvent>();
@@ -212,7 +170,7 @@ export class NosqlFsSource extends ol.source.Vector {
   private composeUrl(extent?: number[]) {
     const params = {
       ...fromNullable(extent).fold({}, v => ({ bbox: v.join(",") })),
-      ...this.view.fold({}, v => ({ "with-view": v })),
+      ...this.view.fold({}, v => ({ "with-view": encodeURIComponent(v) })),
       ...this.filter.fold({}, f => ({ query: encodeURIComponent(f) }))
     };
 
@@ -224,7 +182,7 @@ export class NosqlFsSource extends ol.source.Vector {
   }
 
   fetchFeatures$(extent: number[]): rx.Observable<GeoJsonLike> {
-    return fetchWithTimeout(
+    return fetchWithTimeoutObs$(
       this.composeUrl(extent),
       {
         method: "GET",
@@ -232,11 +190,14 @@ export class NosqlFsSource extends ol.source.Vector {
         credentials: "include" // essentieel om ACM Authenticatie cookies mee te sturen
       },
       FETCH_TIMEOUT
-    ).pipe(switchMap(response => getReceivedFeaturesObs$(this.laagnaam, featureDelimiter, response)));
+    ).pipe(
+      split(featureDelimiter),
+      toGeoJson
+    );
   }
 
   fetchFeaturesByWkt$(wkt: string): rx.Observable<GeoJsonLike> {
-    return fetchWithTimeout(
+    return fetchWithTimeoutObs$(
       this.composeUrl(),
       {
         method: "POST",
@@ -245,7 +206,10 @@ export class NosqlFsSource extends ol.source.Vector {
         body: wkt
       },
       FETCH_TIMEOUT
-    ).pipe(switchMap(response => getReceivedFeaturesObs$(this.laagnaam, featureDelimiter, response)));
+    ).pipe(
+      split(featureDelimiter),
+      toGeoJson
+    );
   }
 
   setOffline(offline: boolean) {
--- projects/ng-kaart/src/lib/util/fetch-with-timeout.ts
+++ projects/ng-kaart/src/lib/util/fetch-with-timeout.ts
@@ -1,10 +1,44 @@
-import { Observable } from "rxjs";
-import { from } from "rxjs";
+import { Function1, Function2, Function3 } from "fp-ts/lib/function";
+import * as rx from "rxjs";
+import { map, switchMap } from "rxjs/operators";
 
-function timeoutPromise(timeout: number): Promise<Response> {
-  return new Promise((_, reject) => setTimeout(() => reject(new Error(`Geen request binnen ${timeout} ms`)), timeout));
-}
+import { Pipeable } from "./operators";
 
-export function fetchWithTimeout(url, options, timeout): Observable<Response> {
-  return from(Promise.race([fetch(url, options), timeoutPromise(timeout)]));
-}
+const decoder = new TextDecoder();
+
+const byteToText: Pipeable<Uint8Array, string> = obs => obs.pipe(map(arr => decoder.decode(arr)));
+
+export const fetchObs$: Function2<string, RequestInit, rx.Observable<string>> = (url, options) =>
+  rx.from(fetch(url, options).then(resp => resp.body.getReader())).pipe(
+    switchMap(readerToObservable),
+    byteToText
+  );
+
+export const fetchWithTimeoutObs$: Function3<string, RequestInit, number, rx.Observable<string>> = (url, options, timeout) =>
+  rx.from(fetchWithTimeout(url, options, timeout).then(resp => resp.body.getReader())).pipe(
+    switchMap(readerToObservable),
+    byteToText
+  );
+
+const readerToObservable: Function1<ReadableStreamReader, rx.Observable<Uint8Array>> = reader =>
+  new rx.Observable(observable => {
+    const push = () =>
+      reader
+        .read()
+        .then(({ done, value }) => {
+          if (!done) {
+            observable.next(value);
+            push();
+          } else {
+            observable.complete();
+          }
+        })
+        .catch(err => observable.error(err));
+    push();
+  });
+
+const timeoutPromise: Function1<number, Promise<Response>> = timeout =>
+  new Promise((_, reject) => setTimeout(() => reject(new Error(`Geen request binnen ${timeout} ms`)), timeout));
+
+const fetchWithTimeout: Function3<string, RequestInit, number, Promise<Response>> = (url, options, timeout) =>
+  Promise.race([fetch(url, options), timeoutPromise(timeout)]);
