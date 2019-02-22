57741852e6d5f46b6f6b8862d93d98714efeee93
Geert Premereur <bepremeg@users.noreply.github.com>
2019-02-22 11:31:55 +0100

    AGL-3022: Meten lengte en oppervlakte
    aangepast: herordend + readonly

--- projects/ng-kaart/src/lib/kaart/tekenen/route.msg.ts
+++ projects/ng-kaart/src/lib/kaart/tekenen/route.msg.ts
@@ -4,10 +4,37 @@ import { Versions } from "./waypoint-ops";
 import { Waypoint, WaypointId } from "./waypoint.msg";
 
 export interface ProtoRoute {
+  readonly id: string;
+  readonly version: number;
+  readonly begin: Waypoint;
+  readonly end: Waypoint;
+}
+
+export interface GeometryRoute {
   id: string;
   version: number;
   begin: Waypoint;
   end: Waypoint;
+  geometry: ol.geom.Geometry;
+}
+
+export type RouteEvent = RouteAdded | RouteRemoved;
+
+export type RouteEventId = string;
+
+export interface RouteAdded {
+  readonly type: "RouteAdded";
+  readonly id: RouteEventId;
+  readonly version: number;
+  readonly startWaypointId: WaypointId; // we moeten weten waar in de volgorde van deelroutes dit thuis hoort om de lengte te kunnen meten
+  readonly geometry: ol.geom.Geometry;
+}
+
+export interface RouteRemoved {
+  readonly type: "RouteRemoved";
+  readonly id: RouteEventId;
+  readonly version: number;
+  readonly startWaypointId: WaypointId;
 }
 
 export function createRoute(begin: Waypoint, end: Waypoint, versions: Versions): ProtoRoute {
@@ -20,26 +47,6 @@ export function createRoute(begin: Waypoint, end: Waypoint, versions: Versions):
   };
 }
 
-export interface GeometryRoute {
-  id: string;
-  version: number;
-  begin: Waypoint;
-  end: Waypoint;
-  geometry: ol.geom.Geometry;
-}
-
-export type RouteEventId = string;
-
-export type RouteEvent = RouteAdded | RouteRemoved;
-
-export interface RouteAdded {
-  readonly type: "RouteAdded";
-  readonly id: RouteEventId;
-  readonly version: number;
-  readonly startWaypointId: WaypointId; // we moeten weten waar in de volgorde van deelroutes dit thuis hoort om de lengte te kunnen meten
-  readonly geometry: ol.geom.Geometry;
-}
-
 export function routeAdded(geometryRoute: GeometryRoute): RouteAdded {
   return {
     type: "RouteAdded",
@@ -48,13 +55,6 @@ export function routeAdded(geometryRoute: GeometryRoute): RouteAdded {
     startWaypointId: geometryRoute.begin.id,
     geometry: geometryRoute.geometry
   };
-}
-
-export interface RouteRemoved {
-  readonly type: "RouteRemoved";
-  readonly id: RouteEventId;
-  readonly version: number;
-  readonly startWaypointId: WaypointId;
 }
 
 export function routeRemoved(protoRoute: ProtoRoute): RouteRemoved {
