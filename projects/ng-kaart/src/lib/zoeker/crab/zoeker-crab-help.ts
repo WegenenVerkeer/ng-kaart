70ff5a4672123b6281f4ab766332e310e8448e11
bauwelp <bauwelp@gmail.com>
2019-02-28 09:22:53 +0100

    CK-41: aanpassingen na test

--- projects/ng-kaart/src/lib/zoeker/crab/zoeker-crab-help.ts
+++ projects/ng-kaart/src/lib/zoeker/crab/zoeker-crab-help.ts
@@ -3,5 +3,5 @@ export const getrapt = `Via de menu kan je kiezen voor 'Zoek op adres via Crab'.
 <br>
 Je hoeft niet voor alle velden een keuze te maken om de zoekopdracht uit te voeren.
 <br>
-Alle data die hier beschikbaar gesteld wordt, komt uit de <a href='https://overheid.vlaanderen.be/informatie-vlaanderen/producten-diensten/centraal-referentieadressenbestand-crab'>CRAB</a> diensten, beheerd door Informatie Vlaanderen.`;
+Alle data die hier beschikbaar gesteld wordt, komt uit de <a target="_blank" href='https://overheid.vlaanderen.be/informatie-vlaanderen/producten-diensten/centraal-referentieadressenbestand-crab'>CRAB</a> diensten, beheerd door Informatie Vlaanderen.`;
 // tslint:enable:max-line-length
