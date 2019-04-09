e121f05920556dc33fe0ff981da996a8e5a84bbe
Geert Premereur <bepremeg@users.noreply.github.com>
2019-04-09 11:31:27 +0200

    AGL-3468: Fetchen van features (#178)
    * AGL-3468: Fetchen van features
    
    * AGL-3468: Feature fetch bug
    
    verwijderd: bijhouden van features in memory om enkel nieuwe op de kaart
    te zetten
    toegevoegd: enkel features van recentste extent op kaart zetten.
    
    * AGL-3468: Feature fetch bug
    
    toegevoegd: documentatie
    
    * AGL-3468: Feature fetch bug
    
    aangepast: documentatie

--- projects/ng-kaart/src/lib/classic/lagen/classic-nosqlfs-laag.component.ts
+++ projects/ng-kaart/src/lib/classic/lagen/classic-nosqlfs-laag.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, NgZone, ViewEncapsulation } from "@angular/core";
 import { option } from "fp-ts";
 import { identity } from "fp-ts/lib/function";
 import { fromNullable } from "fp-ts/lib/Option";
-import * as rx from "rxjs";
 import { switchMap } from "rxjs/operators";
 
 import { kaartLogger } from "../../kaart";
@@ -44,6 +43,14 @@ export class ClassicNosqlfsLaagComponent extends ClassicVectorLaagLikeComponent 
   gebruikCache = false;
   @Input()
   veldinfos: ke.VeldInfo[] = [];
+  /* Deze waarde bepaalt hoeveel features er in geheugen bijgehouden worden (tussen verschillende fetches van de NosqlFS
+     source) om te verhinderen dat ze opnieuw getekend moeten worden bij het verschuiven of uitzoomen van de kaart.
+     Gebruik deze instelling alleen wanneer je weet dat het echt nodig is! Grotere waarden zorgen voor meer
+     geheugengebruik maar features worden potentieel sneller getoond bij uitzoomen en verschuiven van de kaart. Het
+     ideale is om bij de start van de applicatie te detecteren of die op een krachtig toestel aan het lopen is of niet.
+     Voor een telefoon bijvoorbeeld zou dit bijvoorbeeld op 1000 gezet kunenn worden. De beste waarde kan best per
+     applicatie bepaald worden obv performantietests. Sommige applicaties gebruiken features met meer en omvangrijkere
+     properties dan andere. */
   @Input()
   maxFeaturesInMemCache = 2500;
 
