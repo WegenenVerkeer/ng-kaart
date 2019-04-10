312864c9677ceb542badaf1070e2de2995b2b509
Geert Premereur <bepremeg@users.noreply.github.com>
2019-04-10 14:22:26 +0200

    CK-111: Webcomponent
    toegevoegd: documentatie van de bevragen component

--- projects/ng-kaart/src/lib/classic/kaart-bevragen/classic-kaart-bevragen.component.ts
+++ projects/ng-kaart/src/lib/classic/kaart-bevragen/classic-kaart-bevragen.component.ts
@@ -3,6 +3,10 @@ import { Component, Injector } from "@angular/core";
 import { BevraagKaartUiSelector } from "../../kaart/kaart-bevragen/kaart-bevragen.component";
 import { ClassicUIElementSelectorComponentBase } from "../common/classic-ui-element-selector-component-base";
 
+/**
+ * Gebruik deze component om in het linkerpaneel informatie over de coördinaat waar geklikt wordt in de kaart te laten
+ * verschijnen. De coördinaat wordt getoond in Lambert 72 en WGS 84, en is niet configueerbaar.
+ */
 @Component({
   selector: "awv-bevraag-kaart",
   template: ""
