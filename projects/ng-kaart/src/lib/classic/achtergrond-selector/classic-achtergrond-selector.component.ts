2c4c8907f8b27c72b7f03464823b213b4fa9bef3
Geert Premereur <bepremeg@users.noreply.github.com>
2019-04-10 13:01:43 +0200

    CK-111: Webcomponent
    toegevoegd: documentatie achtergrondselectie

--- projects/ng-kaart/src/lib/classic/achtergrond-selector/classic-achtergrond-selector.component.ts
+++ projects/ng-kaart/src/lib/classic/achtergrond-selector/classic-achtergrond-selector.component.ts
@@ -4,6 +4,17 @@ import { kaartLogOnlyWrapper } from "../../kaart/kaart-internal-messages";
 import * as prt from "../../kaart/kaart-protocol";
 import { ClassicBaseComponent } from "../classic-base.component";
 
+/**
+ * Deze component zorgt voor een uitklapbare selector van achtergrondlagen. Welke lagen er in de selector staan hangt af
+ * van de lagen die gedefinieerd zijn in de <code>&lt;awv-kaart-classic-tag&gt;</code>. De achtergrondselector gebruikt
+ * standaard alle lagen van het type <code>&lt;awv-kaart-tilecache-laag&gt;</code>,
+ * <code>&lt;awv-kaart-wmts-laag&gt;</code>, <code>&lt;awv-kaart-ortho-laag&gt;</code> en
+ * <code>&lt;awv-kaart-blanco-laag&gt;</code>. Deze twee laatste zijn trouwens specifiek bedoeld voor de achtergrondlagenkiezer.
+ *
+ * Door de tag <code>groep</code> te zetten op ofwel <code>'Voorgrond.Laag'</code> of <code>'Achtergrond.Laag'</code>,
+ * wordt een laag respectievelijk niet of wel bij de selecteerbare achtergronden opgenomen. Het is doorgaans geen goed
+ * idee om een volledig dekkende laag in de voorgrond te gebruiken als de achtergrondselectie aanwezig is.
+ */
 @Component({
   selector: "awv-kaart-knop-achtergrondlaag-kiezer",
   template: "",
