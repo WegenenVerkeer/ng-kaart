fc170994fd17336cdb77e12ed44c4d9343e61aa8
Natal Vande Casteele <natal@libius.be>
2019-04-29 10:26:47 +0200

    AGL-3489: fixes na MR

--- projects/ng-kaart/src/lib/lagenkiezer/laagmanipulatie.component.ts
+++ projects/ng-kaart/src/lib/lagenkiezer/laagmanipulatie.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, NgZone, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
-import { MatMenuTrigger } from "@angular/material";
+import { MatIconRegistry, MatMenuTrigger } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
 import * as array from "fp-ts/lib/Array";
 import { Function2 } from "fp-ts/lib/function";
 import { Option } from "fp-ts/lib/Option";
@@ -9,15 +10,20 @@ import { distinctUntilChanged, filter, map, sample, shareReplay, startWith, tap 
 import * as fltr from "../filter/filter-model";
 import { KaartChildComponentBase } from "../kaart/kaart-child-component-base";
 import * as ke from "../kaart/kaart-elementen";
-import { FilterTotaal, TotaalOpgehaald } from "../kaart/kaart-elementen";
+import { TotaalOpgehaald } from "../kaart/kaart-elementen";
 import { kaartLogOnlyWrapper } from "../kaart/kaart-internal-messages";
 import * as cmd from "../kaart/kaart-protocol-commands";
 import { KaartComponent } from "../kaart/kaart.component";
 import { observeOnAngular } from "../util/observe-on-angular";
 import { collectOption } from "../util/operators";
 import { atLeastOneTrue, negate } from "../util/thruth";
+import { encodeAsSvgUrl } from "../util/url";
 
 import { LagenkiezerComponent } from "./lagenkiezer.component";
+
+const spinnerSVG =
+  // tslint:disable-next-line:max-line-length
+  '<svg class="lds-spin" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="background: none;"><g transform="translate(80,50)"> <g transform="rotate(0)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="1" transform="scale(1.06586 1.06586)"> <animateTransform attributeName="transform" type="scale" begin="-0.875s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.875s"></animate> </circle> </g> </g><g transform="translate(71.21320343559643,71.21320343559643)"> <g transform="rotate(45)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.875" transform="scale(1.07836 1.07836)"> <animateTransform attributeName="transform" type="scale" begin="-0.75s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.75s"></animate> </circle> </g> </g><g transform="translate(50,80)"> <g transform="rotate(90)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.75" transform="scale(1.09086 1.09086)"> <animateTransform attributeName="transform" type="scale" begin="-0.625s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.625s"></animate> </circle> </g> </g><g transform="translate(28.786796564403577,71.21320343559643)"> <g transform="rotate(135)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.625" transform="scale(1.00336 1.00336)"> <animateTransform attributeName="transform" type="scale" begin="-0.5s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.5s"></animate> </circle> </g> </g><g transform="translate(20,50.00000000000001)"> <g transform="rotate(180)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.5" transform="scale(1.01586 1.01586)"> <animateTransform attributeName="transform" type="scale" begin="-0.375s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.375s"></animate> </circle> </g> </g><g transform="translate(28.78679656440357,28.786796564403577)"> <g transform="rotate(225)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.375" transform="scale(1.02836 1.02836)"> <animateTransform attributeName="transform" type="scale" begin="-0.25s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.25s"></animate> </circle> </g> </g><g transform="translate(49.99999999999999,20)"> <g transform="rotate(270)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.25" transform="scale(1.04086 1.04086)"> <animateTransform attributeName="transform" type="scale" begin="-0.125s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="-0.125s"></animate> </circle> </g> </g><g transform="translate(71.21320343559643,28.78679656440357)"> <g transform="rotate(315)"> <circle cx="0" cy="0" r="8" fill="#000000" fill-opacity="0.125" transform="scale(1.05336 1.05336)"> <animateTransform attributeName="transform" type="scale" begin="0s" values="1.1 1.1;1 1" keyTimes="0;1" dur="1s" repeatCount="indefinite"></animateTransform> <animate attributeName="fill-opacity" keyTimes="0;1" dur="1s" repeatCount="indefinite" values="1;0" begin="0s"></animate> </circle> </g> </g></svg>';
 
 @Component({
   selector: "awv-laagmanipulatie",
@@ -53,8 +59,17 @@ export class LaagmanipulatieComponent extends KaartChildComponentBase implements
   @ViewChild(MatMenuTrigger)
   laagMenuTrigger: MatMenuTrigger;
 
-  constructor(lagenkiezer: LagenkiezerComponent, kaartComponent: KaartComponent, zone: NgZone) {
+  constructor(
+    lagenkiezer: LagenkiezerComponent,
+    kaartComponent: KaartComponent,
+    zone: NgZone,
+    private readonly matIconRegistry: MatIconRegistry,
+    private readonly domSanitize: DomSanitizer
+  ) {
     super(kaartComponent, zone);
+
+    this.matIconRegistry.addSvgIcon("spinner", this.domSanitize.bypassSecurityTrustResourceUrl(encodeAsSvgUrl(spinnerSVG)));
+
     this.zoom$ = kaartComponent.modelChanges.viewinstellingen$.pipe(
       map(zi => zi.zoom),
       distinctUntilChanged(),
