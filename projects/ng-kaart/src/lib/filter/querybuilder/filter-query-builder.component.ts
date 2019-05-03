be33495028e51997747459dcfcbc1dd28d74cd59
Natal Vande Casteele <natal@libius.be>
2019-05-03 16:04:12 +0200

    AGL-3508: incomplete chip toegevoegd

--- projects/ng-kaart/src/lib/filter/querybuilder/filter-query-builder.component.ts
+++ projects/ng-kaart/src/lib/filter/querybuilder/filter-query-builder.component.ts
@@ -8,23 +8,26 @@ import { Filter as fltr } from "../../filter/filter-model";
   styleUrls: ["./filter-query-builder.component.scss"]
 })
 export class FilterQueryBuilderComponent {
-  ident8IsR4 = fltr.Equality(fltr.Property("string", "ident8", "Ident8"), fltr.Literal("string", "R0040001"));
   typeIsAanliggend = fltr.Equality(fltr.Property("string", "type", "Type"), fltr.Literal("string", "Aanliggend"));
   typeIsNietVerhoogd = fltr.Inequality(fltr.Property("string", "type", "Type"), fltr.Literal("string", "Verhoogd"));
+
+  ident8IsR4 = fltr.Equality(fltr.Property("string", "ident8", "Ident8"), fltr.Literal("string", "R0040001"));
   ident8IsR1 = fltr.Equality(fltr.Property("string", "ident8", "Ident8"), fltr.Literal("string", "R0010001"));
   ident8IsR8 = fltr.Equality(fltr.Property("string", "ident8", "Ident8"), fltr.Literal("string", "R0080001"));
+
   incomplete = fltr.Incomplete(fltr.Property("string", "?", "?"), fltr.Literal("string", "?"));
 
   conj1 = fltr.Conjunction(this.ident8IsR4, this.typeIsAanliggend);
   conj2 = fltr.Conjunction(this.conj1, this.typeIsNietVerhoogd);
-  conj3 = fltr.Conjunction(this.conj2, this.ident8IsR1);
-  conj4 = fltr.Conjunction(this.conj3, this.incomplete);
 
-  disj1 = fltr.Disjunction(this.conj4, this.conj3);
+  conj3 = fltr.Conjunction(this.ident8IsR1, this.typeIsAanliggend);
+
+  disj1 = fltr.Disjunction(this.conj2, this.conj3);
   disj2 = fltr.Disjunction(this.disj1, this.ident8IsR8);
+  disj3 = fltr.Disjunction(this.disj2, this.incomplete);
 
   @Input()
-  expression: fltr.Expression = this.disj2;
+  expression: fltr.Expression = this.disj3;
 
   property(): string {
     return (<fltr.Comparison>this.expression).property.ref;
