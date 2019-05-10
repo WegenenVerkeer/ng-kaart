a1fe977b420c992a9ff51b1a993e4083e03a2ad2
Natal Vande Casteele <natal@libius.be>
2019-05-10 09:44:17 +0200

    AGL-3513: laag ook activeren bij aan en uitzetten van filter

--- projects/ng-kaart/src/lib/lagenkiezer/laagmanipulatie.component.ts
+++ projects/ng-kaart/src/lib/lagenkiezer/laagmanipulatie.component.ts
@@ -152,7 +152,13 @@ export class LaagmanipulatieComponent extends KaartChildComponentBase implements
     this.runInViewReady(
       this.filterActief$.pipe(
         sample(toggleFilterActief$),
-        tap(actief => this.dispatch(cmd.ActiveerFilter(this.laag.titel, !actief, kaartLogOnlyWrapper)))
+        tap(actief => {
+          const wordtActief = !actief;
+          this.dispatch(cmd.ActiveerFilter(this.laag.titel, wordtActief, kaartLogOnlyWrapper));
+          if (wordtActief) {
+            this.dispatch(cmd.MaakLaagZichtbaarCmd(this.laag.titel, kaartLogOnlyWrapper));
+          }
+        })
       )
     );
   }
