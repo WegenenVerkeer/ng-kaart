f42dc077c5c93ed31cba6cfe7f42d207a1fbab01
Geert Premereur <bepremeg@users.noreply.github.com>
2019-08-09 09:11:04 +0200

    UX-127: Library Support
    toegevoegd: demo gebruik awv logging (niet bouwbaar op travis)

--- projects/ng-kaart/src/lib/kaart/log.ts
+++ projects/ng-kaart/src/lib/kaart/log.ts
@@ -1,4 +1,18 @@
+import { awvLogging, LogLevel } from "@awv/awv-clientlogging";
 import * as log from "loglevel";
 
 export const kaartLogger = log.getLogger("ng-kaart");
 kaartLogger.setDefaultLevel("info");
+
+const kaartLogger2 = awvLogging.getLoggerForName("ng-kaart");
+kaartLogger2.debug("debug");
+kaartLogger2.info("info");
+kaartLogger2.warn("warn");
+kaartLogger2.error("error");
+
+setTimeout(() => {
+  kaartLogger2.debug("debug");
+  kaartLogger2.info("info");
+  kaartLogger2.warn("warn");
+  kaartLogger2.error("error");
+}, 15000);
