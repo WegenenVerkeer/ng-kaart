85b580aa2d99ebafc6f295a6e2bb7d0479fc6034
Geert Premereur <bepremeg@users.noreply.github.com>
2019-08-19 10:37:58 +0200

    UX-127: client library logging
    aangepast: using the new DSL

--- projects/ng-kaart/src/lib/kaart/log.ts
+++ projects/ng-kaart/src/lib/kaart/log.ts
@@ -1,4 +1,4 @@
-import { awvLogging, LogLevel } from "@awv/awv-clientlogging";
+import { awvLogging, exceptionOption, tagsOption } from "@awv/awv-clientlogging";
 import * as log from "loglevel";
 
 export const kaartLogger = log.getLogger("ng-kaart");
@@ -8,7 +8,8 @@ const kaartLogger2 = awvLogging.getLoggerForName("ng-kaart");
 kaartLogger2.debug("debug");
 kaartLogger2.info("info");
 kaartLogger2.warn("warn");
-kaartLogger2.error("error");
+kaartLogger2.warnWithOptions("warn o", tagsOption("rood", "groen"));
+kaartLogger2.error("error o", { a: 1 }, exceptionOption("Kaboom"));
 
 setTimeout(() => {
   kaartLogger2.debug("debug");
