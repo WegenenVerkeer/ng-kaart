1635ff1f51842aeecd352e808a1ad6ba5f746320
Geert Premereur <bepremeg@users.noreply.github.com>
2019-10-29 19:44:30 +0100

    AGL-3454: Datum filteren
    aangepast: fix unit tests

--- projects/ng-kaart/src/lib/filter/filter-awv0-interpreter.spec.ts
+++ projects/ng-kaart/src/lib/filter/filter-awv0-interpreter.spec.ts
@@ -1,7 +1,10 @@
-import { Function1, Function2 } from "fp-ts/lib/function";
+import { option } from "fp-ts";
+import { eqString } from "fp-ts/lib/Eq";
+import { Function1, Function2, Lazy } from "fp-ts/lib/function";
 import { fromNullable, none, some } from "fp-ts/lib/Option";
 
 import { Consumer1, Consumer2 } from "../util/function";
+import { NoOptionRecord, optionsToUndefined } from "../util/option";
 
 import { AwvV0FilterInterpreters } from "./filter-awv0-interpreter";
 import { Filter as fltr } from "./filter-model";
@@ -19,14 +22,43 @@ const modifyKinded: Consumer2<any, { [p: string]: Consumer1<any> }> = (obj, modi
   return obj;
 };
 
+const isOption = (obj: unknown): obj is option.Option<unknown> =>
+  typeof obj === "object" && (obj["_tag"] === "Some" || obj["_tag"] === "None");
+const ensureOption = (obj: unknown): unknown => (!isOption(obj) ? option.fromNullable(obj) : obj);
+
+// We willen zo weinig mogelijk duplicatie in onze testen. Wat we parsen bevat enkel basis datatypes, geen Options. De
+// output echter, bevat wel Options. Voor de rest is echter alles gelijk. Daarom "fixen" we de velden waarvan we weten
+// dat het Optionals moeten zijn obv de ruwe waarde (als die er is).
 const fixOptionals: Function1<any, fltr.Filter> = rawFilter =>
   (modifyKinded(rawFilter, {
-    ExpressionFilter: ef => (ef.name = fromNullable(ef.name))
+    ExpressionFilter: ef => (ef.name = ensureOption(ef.name)),
+    Property: prop => (prop.sqlFormat = ensureOption(prop.sqlFormat))
   }) as any) as fltr.Filter; // super fishy. gelukkig "maar" een test
 
 describe("De filterinterpreter", () => {
-  const property: fltr.Property = fltr.Property("string", "prop", "Property", "DD/MM/YYYY");
+  // Option is lazy omdat we met fixOptionals de instantie in-place wijzigen. We kunnen die dan niet meer hergebruiken.
+  const property: Lazy<NoOptionRecord<fltr.Property>> = () => optionsToUndefined(fltr.Property("string", "prop", "Property", "DD/MM/YYYY"));
   const literal: fltr.Literal = fltr.Literal("string", "value");
+  beforeEach(() => {
+    // Jasmine kan standaard niet overweg met fp-ts options
+    const isStringSome = (obj: unknown): obj is option.Some<string> =>
+      typeof obj === "object" && obj["_tag"] === "Some" && typeof obj["value"] === "string";
+    const isNone = (obj): obj is option.None<string> => typeof obj === "object" && obj["_tag"] === "None";
+    jasmine.addCustomEqualityTester((opt1, opt2) => {
+      if (isStringSome(opt1) && isStringSome(opt2)) {
+        return option.getEq(eqString).equals(opt1, opt2);
+      } else {
+        return undefined;
+      }
+    });
+    jasmine.addCustomEqualityTester((opt1, opt2) => {
+      if (isNone(opt1) && isNone(opt2)) {
+        return true;
+      } else {
+        return undefined;
+      }
+    });
+  });
   describe("bij het interpreteren van geldige structuren", () => {
     it("moet een 'empty' filter kunnen verwerken", () => {
       const empty: fltr.Filter = fltr.empty();
@@ -41,14 +73,15 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "equality",
-          property: property,
+          property: property(),
           value: literal,
           caseSensitive: false
         }
       };
       const result = AwvV0FilterInterpreters.jsonAwv0Definition(eq);
+      const target = fixOptionals(eq);
       expect(result.isSuccess()).toBe(true);
-      expect(result.getOrElse(undefined)).toEqual(fixOptionals(eq));
+      expect(result.getOrElse(undefined)).toEqual(target);
     });
     it("moet een filter met 1 case sensitive 'gelijk aan' kunnen verwerken", () => {
       const eq = {
@@ -57,7 +90,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "equality",
-          property: property,
+          property: property(),
           value: literal,
           caseSensitive: true
         }
@@ -73,7 +106,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "equality",
-          property: property,
+          property: property(),
           value: literal,
           caseSensitive: false
         }
@@ -89,7 +122,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "inequality",
-          property: property,
+          property: property(),
           value: literal,
           caseSensitive: false
         }
@@ -105,7 +138,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "starts",
-          property: property,
+          property: property(),
           value: literal,
           caseSensitive: false
         }
@@ -121,7 +154,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "UnaryComparison",
           operator: "isEmpty",
-          property: property
+          property: property()
         }
       };
       const result = AwvV0FilterInterpreters.jsonAwv0Definition(ndef);
@@ -135,7 +168,21 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "UnaryComparison",
           operator: "isNotEmpty",
-          property: property
+          property: property()
+        }
+      };
+      const result = AwvV0FilterInterpreters.jsonAwv0Definition(def);
+      expect(result.isSuccess()).toBe(true);
+      expect(result.getOrElse(undefined)).toEqual(fixOptionals(def));
+    });
+    it("moet een filter met 1 'heeft een waarde' kunnen verwerken", () => {
+      const def = {
+        kind: "ExpressionFilter",
+        name: "testFilter",
+        expression: {
+          kind: "UnaryComparison",
+          operator: "isNotEmpty",
+          property: property()
         }
       };
       const result = AwvV0FilterInterpreters.jsonAwv0Definition(def);
@@ -151,14 +198,14 @@ describe("De filterinterpreter", () => {
           left: {
             kind: "BinaryComparison",
             operator: "inequality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           },
           right: {
             kind: "BinaryComparison",
             operator: "equality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           }
@@ -179,14 +226,14 @@ describe("De filterinterpreter", () => {
             left: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             },
             right: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             }
@@ -194,7 +241,7 @@ describe("De filterinterpreter", () => {
           right: {
             kind: "BinaryComparison",
             operator: "equality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           }
@@ -213,14 +260,14 @@ describe("De filterinterpreter", () => {
           left: {
             kind: "BinaryComparison",
             operator: "inequality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           },
           right: {
             kind: "BinaryComparison",
             operator: "equality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           }
@@ -241,14 +288,14 @@ describe("De filterinterpreter", () => {
             left: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             },
             right: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             }
@@ -256,7 +303,7 @@ describe("De filterinterpreter", () => {
           right: {
             kind: "BinaryComparison",
             operator: "equality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           }
@@ -277,14 +324,14 @@ describe("De filterinterpreter", () => {
             left: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             },
             right: {
               kind: "BinaryComparison",
               operator: "equality",
-              property: property,
+              property: property(),
               value: literal,
               caseSensitive: false
             }
@@ -292,7 +339,7 @@ describe("De filterinterpreter", () => {
           right: {
             kind: "BinaryComparison",
             operator: "equality",
-            property: property,
+            property: property(),
             value: literal,
             caseSensitive: false
           }
@@ -326,7 +373,7 @@ describe("De filterinterpreter", () => {
         expression: {
           kind: "BinaryComparison",
           operator: "inequality",
-          property: property,
+          property: property(),
           value: { ...literal, type: "double" },
           caseSensitive: false
         }
