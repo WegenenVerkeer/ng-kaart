79695948d03fe6369620c43f1ca35c8d03220de1
Geert Premereur <bepremeg@users.noreply.github.com>
2019-11-07 17:48:51 +0100

    AGL-3710: kolombreedte (WIP)

--- projects/ng-kaart/src/lib/kaart/feature-tabel/feature-tabel-data.component.ts
+++ projects/ng-kaart/src/lib/kaart/feature-tabel/feature-tabel-data.component.ts
@@ -48,7 +48,9 @@ namespace ColumnHeaders {
     columnWidths: pipe(
       fieldSelections,
       array.map(fs => fs.contributingVeldinfos.length),
-      array.map(numFields => `minmax(${140 + (numFields - 1) * 35}px, 400px)`),
+      array.mapWithIndex(
+        (i, numFields) => `minmax(${140 + (numFields - 1) * 35}px, ${i === fieldSelections.length - 1 ? "1fr" : "400px"})`
+      ),
       join(" ")
     )
   });
