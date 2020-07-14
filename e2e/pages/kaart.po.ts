3d282b9f8ccfc335fb20e9ccfff508f907074ba0
Geert Premereur <bepremeg@users.noreply.github.com>
2020-07-14 15:32:49 +0200

    ELNG-652: Naar Angular 10
    toegevoegd: hack voor Protractor

--- e2e/pages/kaart.po.ts
+++ e2e/pages/kaart.po.ts
@@ -6,6 +6,8 @@ export class KaartPage {
   async gaNaarPagina(): Promise<void> {
     const configuratorKaart: ElementFinder = element(by.id("qa-protractor"));
     await browser.get(`/test`);
-    await configuratorKaart;
+    // De cast hier onder is een hack want ElementFinder is geen Promise. Het
+    // implementeert enkel de then methode.
+    await ((configuratorKaart as unknown) as Promise<void>);
   }
 }
