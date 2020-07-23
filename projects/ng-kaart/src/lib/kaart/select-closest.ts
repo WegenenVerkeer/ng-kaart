99878324b3a9db20d976e44cd496ff3c6955fdc3
Geert Premereur <bepremeg@users.noreply.github.com>
2020-07-23 11:40:52 +0200

    Fix typo

--- projects/ng-kaart/src/lib/kaart/select-closest.ts
+++ projects/ng-kaart/src/lib/kaart/select-closest.ts
@@ -8,10 +8,11 @@ import { distance } from "../util/geometries";
 import * as ol from "../util/openlayers-compat";
 
 //////////////////////////////////////////////////////////////////////////////////
-// Functies om bij een select ta maken die de dichtstbijzijnde feature selecteert.
+// Functies om bij een select de dichtstbijzijnde feature te selecteren.
 //
-// Dit is op basis van een OL multi select waarvan we alle behalve 1 feature (de dichtste) weerhouden. Het probleem met
-// de de native select van OL is dat die de eerste feature (volgens een of andere arbitraire volgorde) selecteert.
+// Dit is op basis van een OL multi select waarvan we alle behalve 1 feature (de
+// dichtste) weerhouden. Het probleem met de native select van OL is dat die de
+// eerste feature (volgens een of andere arbitraire volgorde) selecteert.
 
 const distanceFromPointOrd: Function1<ol.Coordinate, ord.Ord<tuple.Tuple<ol.Feature, ol.geom.Geometry>>> = coord =>
   tuple.getOrd(ord.fromCompare(_ => 0), ord.contramap(geometry => distance(coord, geometry.getClosestPoint(coord)), ord.ordNumber));
