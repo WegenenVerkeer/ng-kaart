f091ebee50d2ded7af708e0256f4d694e473c192
Natal Vande Casteele <natal@libius.be>
2020-10-29 12:53:59 +0100

    AGL-4005: servicenow case onmiddelijk aanmaken

--- projects/ng-kaart/src/lib/servicenow/servicenow-maak-case.component.ts
+++ projects/ng-kaart/src/lib/servicenow/servicenow-maak-case.component.ts
@@ -1,7 +1,5 @@
-import { animate, style, transition, trigger } from "@angular/animations";
 import { HttpClient } from "@angular/common/http";
-import { Component, Input } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import { ChangeDetectorRef, Component, Input } from "@angular/core";
 import { copyToClipboard } from "../util/clipboard";
 
 import { kaartLogger } from "../kaart/log";
@@ -15,24 +13,6 @@ interface ServiceNowResultaat {
   selector: "awv-servicenow-maak-case",
   templateUrl: "./servicenow-maak-case.component.html",
   styleUrls: ["./servicenow-maak-case.component.scss"],
-  animations: [
-    trigger("enterAnimation", [
-      transition(":enter", [
-        style({ opacity: 0, "max-height": 0 }),
-        animate(
-          "0.35s cubic-bezier(.62,.28,.23,.99)",
-          style({ opacity: 1, "max-height": "400px" })
-        ),
-      ]),
-      transition(":leave", [
-        style({ opacity: 1, "max-height": "400px" }),
-        animate(
-          "0.35s cubic-bezier(.62,.28,.23,.99)",
-          style({ opacity: 0, "max-height": 0 })
-        ),
-      ]),
-    ]),
-  ],
 })
 export class ServicenowMaakCaseComponent {
   @Input()
@@ -41,26 +21,20 @@ export class ServicenowMaakCaseComponent {
   serviceNowSuccessBoodschap = "";
   serviceNowFoutBoodschap = "";
   caseNummer: string | undefined;
-
-  toonServiceNowCaseForm = false;
   versturenBezig = false;
-  serviceNowInputControl = new FormControl("", [Validators.required]);
 
-  constructor(private http: HttpClient) {}
-
-  toggleToonCaseForm() {
-    this.toonServiceNowCaseForm = !this.toonServiceNowCaseForm;
-    this.serviceNowInputControl.setValue("");
-  }
+  constructor(
+    private http: HttpClient,
+    private readonly cdr: ChangeDetectorRef
+  ) {}
 
   verstuurMelding() {
-    const boodschap = this.serviceNowInputControl.value;
+    // const boodschap = this.serviceNowInputControl.value;
 
     this.versturenBezig = true;
     this.http
       .post<ServiceNowResultaat>("/geoloket/rest/servicenow/maak/case", {
         installatieId: this.installatieId,
-        omschrijving: boodschap,
       })
       .subscribe(
         (res) => {
@@ -68,14 +42,15 @@ export class ServicenowMaakCaseComponent {
           this.serviceNowSuccessBoodschap = res.boodschap;
           this.serviceNowFoutBoodschap = "";
           this.caseNummer = res.caseNummer;
-          this.toggleToonCaseForm();
+          this.cdr.detectChanges();
         }, //
         (err) => {
           this.versturenBezig = false;
-          const errorboodschap = `Kon ServiceNow case niet aanmaken: ${err.boodschap}`;
+          const errorboodschap = err?.error?.boodschap;
           this.serviceNowSuccessBoodschap = "";
           this.serviceNowFoutBoodschap = errorboodschap;
           kaartLogger.error(errorboodschap);
+          this.cdr.detectChanges();
         }
       );
   }
